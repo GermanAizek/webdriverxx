57635a65e8af7c2acc43f2bc83c3550e844f8570
GermanAizek <GermanAizek@yandex.ru>
2021-03-17 11:02:52 +0300

    Refactor style code

--- src/include/webdriverxx/browsers/chrome.h
+++ src/include/webdriverxx/browsers/chrome.h
@@ -8,12 +8,12 @@ namespace webdriverxx {
 namespace chrome {
 namespace device {
 	std::list<std::string> deviceList {
-        "BlackBerry Z30",
-        "Blackberry PlayBook",
-        "Galaxy Note 3",
-        "Galaxy Note II",
-        "Galaxy S III",
-        "Kindle Fire HDX",
+		"BlackBerry Z30",
+		"Blackberry PlayBook",
+		"Galaxy Note 3",
+		"Galaxy Note II",
+		"Galaxy S III",
+		"Kindle Fire HDX",
 		"LG Optimus L70",
 		"Laptop with HiDPI screen",
 		"Laptop with MDPI screen",
@@ -44,32 +44,32 @@ namespace device {
 		"iPad Pro",
 		"Surface Duo",
 		"Galaxy Fold"
-    };
+	};
 	
 	struct deviceMetrics : JsonObject {
 		WEBDRIVERXX_PROPERTIES_BEGIN(deviceMetrics)
-		WEBDRIVERXX_PROPERTY(width, "width", int)
-		WEBDRIVERXX_PROPERTY(height, "height", int)
+		WEBDRIVERXX_PROPERTY(width,      "width",      int)
+		WEBDRIVERXX_PROPERTY(height,     "height",     int)
 		WEBDRIVERXX_PROPERTY(pixelRatio, "pixelRatio", float)
-		WEBDRIVERXX_PROPERTY(touch, "touch", bool)
+		WEBDRIVERXX_PROPERTY(touch,      "touch",      bool)
 		WEBDRIVERXX_PROPERTIES_END()
 	};
 }
 
 struct MobileEmulation : JsonObject {
 	WEBDRIVERXX_PROPERTIES_BEGIN(MobileEmulation)
-	WEBDRIVERXX_PROPERTY(deviceName, "deviceName", std::string)
-	WEBDRIVERXX_PROPERTY(userAgent, "userAgent", std::string)
+	WEBDRIVERXX_PROPERTY(deviceName,    "deviceName",    std::string)
+	WEBDRIVERXX_PROPERTY(userAgent,     "userAgent",     std::string)
 	WEBDRIVERXX_PROPERTY(deviceMetrics, "deviceMetrics", device::deviceMetrics)
 	WEBDRIVERXX_PROPERTIES_END()
 };
 
 struct PerfLoggingPrefs : JsonObject {
 	WEBDRIVERXX_PROPERTIES_BEGIN(PerfLoggingPrefs)
-	WEBDRIVERXX_PROPERTY(EnableNetwork, "enableNetwork", bool)
-	WEBDRIVERXX_PROPERTY(enablePage, "enablePage", bool)
-	WEBDRIVERXX_PROPERTY(enableTimeline, "enableTimeline", bool)
-	WEBDRIVERXX_PROPERTY(tracingCategories, "tracingCategories", std::string)
+	WEBDRIVERXX_PROPERTY(EnableNetwork,                "enableNetwork",                bool)
+	WEBDRIVERXX_PROPERTY(enablePage,                   "enablePage",                   bool)
+	WEBDRIVERXX_PROPERTY(enableTimeline,               "enableTimeline",               bool)
+	WEBDRIVERXX_PROPERTY(tracingCategories,            "tracingCategories",            std::string)
 	WEBDRIVERXX_PROPERTY(bufferUsageReportingInterval, "bufferUsageReportingInterval", int)
 	WEBDRIVERXX_PROPERTIES_END()
 };
@@ -86,18 +86,18 @@ struct Chrome : Capabilities { // copyable
 
 	// See https://sites.google.com/a/chromium.org/chromedriver/capabilities for details
 	WEBDRIVERXX_PROPERTIES_BEGIN(Chrome)
-	WEBDRIVERXX_PROPERTY(LoggingPrefs, "loggingPrefs", LoggingPrefs)
-	WEBDRIVERXX_PROPERTY(Args, "args", std::vector<std::string>)
-	WEBDRIVERXX_PROPERTY(Binary, "binary", std::string)
+	WEBDRIVERXX_PROPERTY(LoggingPrefs,     "loggingPrefs",     LoggingPrefs)
+	WEBDRIVERXX_PROPERTY(Args,             "args",             std::vector<std::string>)
+	WEBDRIVERXX_PROPERTY(Binary,           "binary",           std::string)
 	// Each extension is a base64-encoded .crx file
-	WEBDRIVERXX_PROPERTY(Extensions, "extensions", std::vector<std::string>)
-	WEBDRIVERXX_PROPERTY(LocalState, "localState", JsonObject)
-	WEBDRIVERXX_PROPERTY(Prefs, "prefs", JsonObject)
-	WEBDRIVERXX_PROPERTY(Detach, "detach", bool)
-	WEBDRIVERXX_PROPERTY(DebuggerAddress, "debuggerAddress", std::string)
-	WEBDRIVERXX_PROPERTY(ExcludeSwitches, "excludeSwitches", std::vector<std::string>)
-	WEBDRIVERXX_PROPERTY(MinidumpPath, "minidumpPath", std::string)
-	WEBDRIVERXX_PROPERTY(MobileEmulation, "mobileEmulation", chrome::MobileEmulation)
+	WEBDRIVERXX_PROPERTY(Extensions,       "extensions",       std::vector<std::string>)
+	WEBDRIVERXX_PROPERTY(LocalState,       "localState",       JsonObject)
+	WEBDRIVERXX_PROPERTY(Prefs,            "prefs",            JsonObject)
+	WEBDRIVERXX_PROPERTY(Detach,           "detach",           bool)
+	WEBDRIVERXX_PROPERTY(DebuggerAddress,  "debuggerAddress",  std::string)
+	WEBDRIVERXX_PROPERTY(ExcludeSwitches,  "excludeSwitches",  std::vector<std::string>)
+	WEBDRIVERXX_PROPERTY(MinidumpPath,     "minidumpPath",     std::string)
+	WEBDRIVERXX_PROPERTY(MobileEmulation,  "mobileEmulation",  chrome::MobileEmulation)
 	WEBDRIVERXX_PROPERTY(PerfLoggingPrefs, "perfLoggingPrefs", chrome::PerfLoggingPrefs)
 	WEBDRIVERXX_PROPERTIES_END()
 };
