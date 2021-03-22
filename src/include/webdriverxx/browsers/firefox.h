5b1605efff1480598be2e9f74b92c516bbff314e
GermanAizek <GermanAizek@yandex.ru>
2021-03-22 17:38:36 +0300

    Created FirefoxProfile settings

--- src/include/webdriverxx/browsers/firefox.h
+++ src/include/webdriverxx/browsers/firefox.h
@@ -4,6 +4,17 @@
 #include "../capabilities.h"
 
 namespace webdriverxx {
+	
+struct FirefoxProfile : JsonObject {
+	WEBDRIVERXX_PROPERTIES_BEGIN(FirefoxProfile)
+	WEBDRIVERXX_PROPERTY(AcceptUntrustedCerts,  "webdriver_accept_untrusted_certs",  bool)
+	WEBDRIVERXX_PROPERTY(AssumeUntrustedIssuer, "webdriver_assume_untrusted_issuer", bool)
+	WEBDRIVERXX_PROPERTY(LogDriver,             "webdriver.log.driver",              log_level::Value)
+	WEBDRIVERXX_PROPERTY(LogFile,               "webdriver.log.file",                std::string)
+	WEBDRIVERXX_PROPERTY(LoadStrategy,          "webdriver.load.strategy",           std::string)
+	WEBDRIVERXX_PROPERTY(LogFile,               "webdriver_firefox_port",            int) // default 7055
+	WEBDRIVERXX_PROPERTIES_END()
+};
 
 struct Firefox : Capabilities { // copyable
 	Firefox(const Capabilities& defaults = Capabilities())
@@ -14,12 +25,12 @@ struct Firefox : Capabilities { // copyable
 	}
 
 	WEBDRIVERXX_PROPERTIES_BEGIN(Firefox)
+	// Profile is a profile folder, zipped and base64 encoded.
+	// base64 string FirefoxProfile : JsonObject
+	WEBDRIVERXX_PROPERTY(Profile,             "firefox_profile",     std::string)
 	WEBDRIVERXX_PROPERTY(LoggingPrefs,        "loggingPrefs",        LoggingPrefs)
 	WEBDRIVERXX_PROPERTY(FirefoxBinary,       "firefox_binary",      std::string)
 	WEBDRIVERXX_PROPERTY(PageLoadingStrategy, "pageLoadingStrategy", std::string)
-	// Profile is a profile folder, zipped and base64 encoded.
-	// TODO: add FirefoxProfile
-	WEBDRIVERXX_PROPERTY(Profile,             "firefox_profile",     std::string)
 	WEBDRIVERXX_PROPERTIES_END()
 };
 
