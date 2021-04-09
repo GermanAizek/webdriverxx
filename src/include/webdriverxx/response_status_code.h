f3c6d408cbd2cdef12f9682b60ef29dacb4d0331
GermanAizek <GermanAizek@yandex.ru>
2021-04-09 23:45:04 +0300

    Added more details on exceptions

--- src/include/webdriverxx/response_status_code.h
+++ src/include/webdriverxx/response_status_code.h
@@ -6,7 +6,6 @@ namespace response_status_code {
 
 enum Value {
 	kSuccess = 0,
-	kNoSuchDriver = 6,
 	kNoSuchElement = 7,
 	kNoSuchFrame = 8,
 	kUnknownCommand = 9,
@@ -35,20 +34,67 @@ enum Value {
 inline
 const char* ToString(Value code) {
 	switch(code) {
-	case kSuccess: return "The command executed successfully.";
-	case kNoSuchDriver: return "A session is either terminated or not started"; 
-	case kNoSuchElement: return "An element could not be located on the page using the given search parameters.";
-	case kNoSuchFrame: return "A request to switch to a frame could not be satisfied because the frame could not be found.";
-	case kUnknownCommand: return "The requested resource could not be found, or a request was received using an HTTP method that is not supported by the mapped resource.";
-	case kStaleElementReference: return "An element command failed because the referenced element is no longer attached to the DOM.";
-	case kElementNotVisible: return "An element command could not be completed because the element is not visible on the page.";
-	case kInvalidElementState: return "An element command could not be completed because the element is in an invalid state (e.g. attempting to click a disabled element).";
-	case kUnknownError: return "An unknown server-side error occurred while processing the command.";
-	case kElementIsNotSelectable: return "An attempt was made to select an element that cannot be selected.";
-	case kJavaScriptError: return "An error occurred while executing user supplied JavaScript.";
-	case kXPathLookupError: return "An error occurred while searching for an element by XPath.";
-	case kTimeout: return "An operation did not complete before its timeout expired.";
-	case kNoSuchWindow: return "A request to switch to a different window could not be satisfied because the window could not be found.";
+	case kSuccess:
+		return "";
+	case kNoSuchElement:
+		return "\
+Thrown when element could not be found.\n\
+If you encounter this exception, you may want to check the following:\n\
+	* Check your selector used in your driver.FindElement(By...(...))\n\
+	* Element may not yet be on the screen at the time of the find operation,\n\
+	  (webpage is still loading) see WaitForValue(...)\n\
+	  for how to write a wait wrapper to wait for an element to appear.\
+	";
+	case kNoSuchFrame:
+		return "Thrown when frame target to be switched doesn't exist.";
+	case kUnknownCommand:
+		return "The requested resource could not be found, or a request was received using an HTTP method that is not supported by the mapped resource.";
+	case kStaleElementReference:
+		return "\
+Thrown when a reference to an element is now \"stale\".\n\n\
+Stale means the element no longer appears on the DOM of the page.\n\n\n\
+Possible causes of StaleElementReferenceException include, but not limited to:\n\
+	* You are no longer on the same page, or the page may have refreshed since the element\n\
+	  was located.\n\
+	* The element may have been removed and re-added to the screen, since it was located.\n\
+	  Such as an element being relocated.\n\
+	  This can happen typically with a javascript framework when values are updated and the\n\
+	  node is rebuilt.\n\
+	* Element may have been inside an iframe or another context which was refreshed.\
+	";
+	case kElementNotVisible:
+		return "\
+Thrown when an element is present on the DOM, but\n\
+it is not visible, and so is not able to be interacted with.\n\n\
+Most commonly encountered when trying to click or read text\n\
+of an element that is hidden from view.\
+	";
+	case kInvalidElementState:
+		return "\
+Thrown when a command could not be completed because the element is in an invalid state.\n\n\
+This can be caused by attempting to clear an element that isn't both editable and resettable.\
+		";
+	case kUnknownError:
+		return "An unknown server-side error occurred while processing the command.";
+	case kElementIsNotSelectable:
+		return "\
+Thrown when trying to select an unselectable element.\n\n\
+For example, selecting a 'script' element.\
+		";
+	case kJavaScriptError:
+		return "An error occurred while executing JavaScript supplied by the user.";
+	case kXPathLookupError:
+		return "An error occurred while searching for an element by XPath.";
+	case kTimeout:
+		return "Thrown when a command does not complete in enough time.";
+	case kNoSuchWindow:
+		return "\
+Thrown when window target to be switched doesn't exist.\n\n\
+To find the current set of active window handles, you can get a list\n\
+of the active window handles in the following way:\n\n\
+	std::cout << driver.GetWindows()[0].GetHandle();\n\
+		";
+	// TODO: Add more info excepts
 	case kInvalidCookieDomain: return "An illegal attempt was made to set a cookie under a different domain than the current page.";
 	case kUnableToSetCookie: return "A request to set a cookie's value could not be satisfied.";
 	case kUnexpectedAlertOpen: return "A modal dialog was open, blocking this operation";
