41b46886bc7dfd211bc0da3fadd3a13c9cf47ea5
GermanAizek <GermanAizek@yandex.ru>
2021-05-25 03:00:52 -0400

    Microadd code

--- src/include/webdriverxx/detail/mobile.h
+++ src/include/webdriverxx/detail/mobile.h
@@ -1,6 +1,7 @@
 #ifndef RELATIVEBY_H
 #define RELATIVEBY_H
 
+#include "types.h"
 #include <string>
 
 namespace webdriverxx {
@@ -10,11 +11,12 @@ public:
     ConnectionType(uint64_t mask) : mask_(mask) {}
 
     void setMask() { mask_ = mask; }
+	uint64_t getMask() { return mask_; }
     bool getAirplaneMode() { return (mask_ % 2) == 1; }
     bool getWifi() { return (mask_ % 2) == 1; }
     bool getData() { return (mask_ / 4) > 0; }
 private:
-    uint64_t mask_;
+	uint64_t mask_;
 };
 
 class Mobile {
@@ -22,6 +24,17 @@ public:
     Mobile()  {
         // TODO: this is not happy end (͡° ͜ʖ ͡°)
     }
+
+	ConnectionType getNetworkConnection() {
+		//return ConnectionType(resource_->GetString("getNetworkConnection"));
+	}
+
+	ConnectionType setNetworkConnection(ConnectionType network) {
+		std::string mode = std::to_string(network.getMask());
+		//mode = is_instance(network) ? : network;
+		// test pls
+		//return
+	}
 private:
     ConnectionType ALL_NETWORK;
     ConnectionType WIFI_NETWORK;
